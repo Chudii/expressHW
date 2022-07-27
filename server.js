d8871ed211fa75a286fffa40b1b87480b5457e71
Chudi <chudiibida3@gmail.com>
2022-07-27 12:38:04 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`<h2>What's up, ${req.params.name.toUpperCase()}? It's nice to see you</h2>`)
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`<h3>From $${req.params.total}: A ${req.params.tipPercentage}% tip would come out to $${(req.params.total) * (req.params.tipPercentage / 100.0)}</h3>`)
+})
+
 app.listen(port, () => {
     console.log(`*** Listening on http://localhost:${port} ***`)
 })
